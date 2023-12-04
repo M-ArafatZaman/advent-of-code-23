2b51fd3ecb2dc5b0939e88241cc06b88ab5e3a69
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-12-04 02:05:31 -0330

    optimize

--- 4/b.py
+++ 4/b.py
@@ -12,9 +12,8 @@ for card in range(len(a)):
         if n in win:
             curr += 1
 
-    for j in range(copies[card]):
-        for i in range(card+1, card+curr+1):
-            copies[i] += 1
+    for i in range(card+1, card+curr+1):
+        copies[i] += copies[card]
 
 s = 0
 for i in copies:
