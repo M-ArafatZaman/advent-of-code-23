3c593c24c79d8646e7fa1c66f5a137679ff562cf
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-12-06 03:30:12 -0330

    Converted part a to constant time

--- 6/a.py
+++ 6/a.py
@@ -1,4 +1,5 @@
 import sys
+from math import ceil, sqrt
 
 a = [i.strip() for i in sys.stdin.readlines()]
 time = [int(j) for j in a[0].split(":")[1].strip().split(" ") if j != ""]
@@ -6,17 +7,9 @@ dist = [int(j) for j in a[1].split(":")[1].strip().split(" ") if j != ""]
 
 s = 1
 for i in range(len(time)):
-    lt, gt = None, None
-    low_speed, high_speed = 0, time[i]
-    low_achieved_dist, high_achieved_dist = 0, 0
-    while (low_achieved_dist <= dist[i] and low_speed <= time[i]) or (high_achieved_dist <= dist[i] and high_speed >= 0):
-        low_achieved_dist = (time[i]-low_speed)*low_speed
-        high_achieved_dist = (time[i]-high_speed)*high_speed
-        if low_achieved_dist > dist[i] and not lt: lt = low_speed
-        if high_achieved_dist > dist[i] and not gt: gt = high_speed
-        low_speed += 1
-        high_speed -= 1
-
-    s *= (gt-lt+1)
+    # s^2 - t*s + d => t+sqrt(t^2 - 4 * 1*d)/2
+    n = ceil( (time[i]-sqrt((time[i]**2)-(4*(dist[i]+.00001))))/2 )
+    lowest, greatest = n, time[i]-n
+    s *= (greatest-lowest+1)
 print(s)
     
