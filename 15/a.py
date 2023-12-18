f8928a8d8887dc35f332651b20b06a8a77875d03
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-12-18 00:54:43 -0330

    sum can take generators

--- 15/a.py
+++ 15/a.py
@@ -1,2 +1,2 @@
 from functools import reduce
-print(sum(list(map(lambda s: reduce(lambda curr, v: (17*(curr+ord(v))) % 256, list(s), 0), input().split(",")))))
+print(sum(map(lambda s: reduce(lambda curr, v: (17*(curr+ord(v))) % 256, list(s), 0), input().split(","))))
