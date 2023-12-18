4ca5eb70ad59bc6d5c7b49781089080e348e4f70
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-12-18 00:52:36 -0330

    Remove data

--- 15/a.py
+++ 15/a.py
@@ -1,3 +1,2 @@
 from functools import reduce
-data = list(map(lambda s: reduce(lambda curr, v: (17*(curr+ord(v))) % 256, list(s), 0), input().split(",")))
-print(sum(data))
+print(sum(list(map(lambda s: reduce(lambda curr, v: (17*(curr+ord(v))) % 256, list(s), 0), input().split(",")))))
